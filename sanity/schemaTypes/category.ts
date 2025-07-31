885a7f1df227b8240dce70ef39b34cda5a7430d6
BLOUDAN JEWELLERY <161647023+BLOUDANJEWELLERY@users.noreply.github.com>
2025-07-31 11:07:11 +0000

    new file:   sanity/.gitignore 	new file:   sanity/README.md 	new file:   sanity/eslint.config.mjs 	new file:   sanity/package-lock.json 	new file:   sanity/package.json 	new file:   sanity/sanity.cli.ts 	new file:   sanity/sanity.config.ts 	new file:   sanity/schemaTypes/category.ts 	new file:   sanity/schemaTypes/index.ts 	new file:   sanity/schemaTypes/product.ts 	new file:   sanity/static/.gitkeep 	new file:   sanity/tsconfig.json

---
+++ sanity/schemaTypes/category.ts
@@ -0,0 +1,30 @@
+// schemas/category.ts
+export default {
+  name: 'category',
+  title: 'Category',
+  type: 'document',
+  fields: [
+    {
+      name: 'name',
+      title: 'Category Name',
+      type: 'string',
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'name',
+        maxLength: 90,
+      },
+    },
+    {
+      name: 'image',
+      title: 'Category Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+  ],
+};
---
+++ sanity/schemaTypes/index.ts
@@ -0,0 +1,5 @@
+// schemas/index.ts
+import category from './category';
+import product from './product';
+
+export const schemaTypes = [category, product];
