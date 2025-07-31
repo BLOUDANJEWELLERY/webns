255493a47e4c149ed470fb9223dcb163406aa594
BLOUDAN JEWELLERY <161647023+BLOUDANJEWELLERY@users.noreply.github.com>
2025-07-31 17:17:42 +0300

    Update product.ts

--- sanity/schemaTypes/product.ts
+++ sanity/schemaTypes/product.ts
@@ -1,11 +1,20 @@
 export default {
-    name: 'product',
-    title: 'Product',
-    type: 'document',
-    fields: [
-        { name: 'title', title: 'Title', type: 'string' },
-        { name: 'price', title: 'Price', type: 'number' },
-        { name: 'image', title: 'Image', type: 'image' },
-        { name: 'description', title: 'Description', type: 'text' },
-    ]
+  name: 'product',
+  title: 'Product',
+  type: 'document',
+  fields: [
+    { name: 'title', title: 'Title', type: 'string' },
+    { name: 'price', title: 'Price', type: 'number' },
+    { name: 'image', title: 'Image', type: 'image' },
+    { name: 'description', title: 'Description', type: 'text' },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+        maxLength: 96,
+      },
+    },
+  ],
 }
