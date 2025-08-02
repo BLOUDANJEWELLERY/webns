54bd9e0ffe3cc0f510aa0261d40af0a2088c132d
BLOUDAN JEWELLERY <161647023+BLOUDANJEWELLERY@users.noreply.github.com>
2025-08-02 16:58:34 +0300

    Update index.ts

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -4,4 +4,4 @@ import category from './category';
 import collection from './collections';
 import product from './product';
 
-export const schemaTypes = [category, product, collection];
+export const schemaTypes = [user, category, product, collection];
