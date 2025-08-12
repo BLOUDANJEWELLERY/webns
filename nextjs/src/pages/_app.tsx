93de200ebf4f7f977f94f74782053d7ce7cc3a2a
BLOUDAN JEWELLERY <161647023+BLOUDANJEWELLERY@users.noreply.github.com>
2025-08-12 12:23:01 +0300

    Update _app.tsx

--- nextjs/src/pages/_app.tsx
+++ nextjs/src/pages/_app.tsx
@@ -1,27 +1,33 @@
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 import { CartProvider } from '../context/CartContext'
 import { Toaster } from 'react-hot-toast'
 import '../styles/details.module.css' // or your main global styles
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <CartProvider>
-      <Toaster
-        position="bottom-center"
-        toastOptions={{
-          style: {
-            background: '#fffaf2', // warm background
-            color: '#5d4037',      // rich brown text
-            border: '1px solid #d7ccc8', // soft brown border
-            fontWeight: 'bold',
-            fontFamily: 'inherit',
-          },
-          success: {
-            icon: '🛒',
-          },
-        }}
-      />
-      <Component {...pageProps} />
-    </CartProvider>
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <CartProvider>
+        <Toaster
+          position="bottom-center"
+          toastOptions={{
+            style: {
+              background: '#fffaf2', // warm background
+              color: '#5d4037',      // rich brown text
+              border: '1px solid #d7ccc8', // soft brown border
+              fontWeight: 'bold',
+              fontFamily: 'inherit',
+            },
+            success: {
+              icon: '🛒',
+            },
+          }}
+        />
+        <Component {...pageProps} />
+      </CartProvider>
+    </>
   )
 }
