0378d46a7ef6380ccc91498933a97a6f97d7edb2
BLOUDAN JEWELLERY <161647023+BLOUDANJEWELLERY@users.noreply.github.com>
2025-08-15 23:04:03 +0300

    Update create.tsx

--- nextjs/src/pages/admin/create.tsx
+++ nextjs/src/pages/admin/create.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 import { useRouter } from 'next/router'
-import styles from '../../styles/adminCreate.module.css'
+import styles from '../../styles/admincreate.module.css'
 
 export default function CreateProduct() {
   const [title, setTitle] = useState('')
