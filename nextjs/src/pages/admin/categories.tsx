be5f47e25f01eeb50691bf5193eb50bfd7c68a7c
BLOUDAN JEWELLERY <161647023+BLOUDANJEWELLERY@users.noreply.github.com>
2025-08-23 11:01:55 +0300

    Update categories.tsx

--- nextjs/src/pages/admin/categories.tsx
+++ nextjs/src/pages/admin/categories.tsx
@@ -21,7 +21,7 @@ import { CSS } from '@dnd-kit/utilities'
 interface CategoryRaw {
   _id: string
   title: string
-  parent?: { _id: string; title }
+  parent?: { _id: string; title: string } 
   order?: number
 }
 
