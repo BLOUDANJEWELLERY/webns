8c6b7b3359cd40d2c4ef6ced3f1d8e50b3806d93
BLOUDAN JEWELLERY <161647023+BLOUDANJEWELLERY@users.noreply.github.com>
2025-08-27 09:15:28 +0300

    Create experimental.tsx

---
+++ nextjs/src/pages/experimental.tsx
@@ -0,0 +1,24 @@
+// pages/somepage.tsx
+import FilterSortModal from "/components/FilterSortModal"
+import { client } from "../lib/sanityClient"
+
+export async function getServerSideProps() {
+  const categories = await client.fetch(`
+    *[_type=="category"]{
+      _id,
+      title,
+      parent->{_id, title},
+      order
+    } | order(order asc)
+  `)
+  return { props: { categories: categories || [] } }
+}
+
+export default function SomePage({ categories }) {
+  return (
+    <div>
+      <h1>Products Page</h1>
+      <FilterSortModal initialCategories={categories} />
+    </div>
+  )
+}
