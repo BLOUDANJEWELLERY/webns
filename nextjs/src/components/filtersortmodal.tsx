dea6be922eae764dc30bad0b323bfd2f89c4fda7
BLOUDAN JEWELLERY <161647023+BLOUDANJEWELLERY@users.noreply.github.com>
2025-08-30 17:55:02 +0300

    Update filtersortmodal.tsx

--- nextjs/src/components/filtersortmodal.tsx
+++ nextjs/src/components/filtersortmodal.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useState, useMemo, useEffect } from 'react'
-import styles from '../../styles/filter.module.css'
+import styles from '../styles/filter.module.css'
 
 interface CategoryRaw {
   _id: string
